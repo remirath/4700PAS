2294a1da9ea4cbca0514215e804820d0069786b4
Ryan <rywood71@gmail.com>
2025-02-07 17:25:15 -0500

    PA4

--- laplace.m
+++ laplace.m
@@ -1,38 +1,58 @@
 % PA 4 Ryan Wood 
+% 101195542
 
 n_nodes = 100;                  % number of nodes on each side of square
 size = 10;                      % size of square
 n_iter = 100;                    % number of iterations
 
-Vx = linspace(0,size,n_nodes);  % 
-Vy = linspace(0,size,n_nodes);
+Vx = linspace(0,size,n_nodes);  % plate mesh x
+Vy = linspace(0,size,n_nodes);  % plate mesh y
 
-dx = size/(n_nodes-1);
-dy = size/(n_nodes-1);
-n = 2:n_nodes-1;
-m = 2:n_nodes-1;
+dx = size/(n_nodes-1);          % minimum x step
+dy = size/(n_nodes-1);          % y step
+n = 2:n_nodes-1;                % number of nodes on mesh in y
+m = 2:n_nodes-1;                % in x
 
 V = zeros(n_nodes, n_nodes);
+E = zeros(n_nodes, n_nodes);
 
-M(n_iter) = struct('cdata',[],'colormap',[]);
-f = figure;
+% M(n_iter) = struct('cdata',[],'colormap',[]);
+% f = figure;
 % f.Visible = 'off';
-axis manual
-axis([0,10,0,10])
-hold on
+axis([0,size,0,size])                   % set axis to size
+% ax.NextPlot = 'replaceChildren';
 
-for j = 1:n_iter
-    V(m,n) = ((V(m+1,n)-2*V(m,n)+V(m-1,n))/dx^2)+((V(m,n-1)-2*V(m,n)+V(m,n+1))/dy^2)
+for j = 1:n_iter                        % update the equation for the number of iterations specified
+    
+    V(m,n) = (V(m+1,n)+V(m-1,n)+V(m,n+1)+V(m,n-1))/4;   % update equation for voltage
+    E = -1*gradient(V,dx);                              % -1*gradient of voltage is electric field
 
-    V(:,1) = 1;
-    V(1,:) = 1;
-    V(n_nodes,:) = 1;
-    V(:,n_nodes) = 1;
+    % Boundary conditions
+    V(:,1) = 1.5;             % Left
+    V(1,:) = 0;             % Bottom
+    V(n_nodes,:) = 0;       % Top
+    V(:,n_nodes) = 1.5;       % Right
+    
+    subplot(2,1,1)
+    surf(Vx,Vy,V)           % surf plot the distribution of charge
+    title('Voltage')
+    view(0,90)              % set view to face-down
+    shading interp
+    colorbar
+    % imboxfilt(V, 5, 'NormalizationFactor',1);
+    drawnow
 
-    surf(Vx,Vy,V)
+    subplot(2,1,2)
+    surf(Vx,Vy,E)           % surface plot the electric field
+    title('Electric Field')
+    view(0,90)
+    shading interp
+    colorbar
+    % imboxfilt(E, 5, 'NormalizationFactor',1);
+    drawnow
 
-    M(j) = getframe;
+    % M(j) = getframe;        % capture the plot as a movie frame
 end
 
-f.Visible = 'on';
-
+% f.Visible = 'on';
+% movie(M,1,20);
